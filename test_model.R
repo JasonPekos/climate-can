f625fea3a6d8b77627811f32353cafd6dc553fd6
Abhiroop24 <abhiroop125@gmail.com>
2023-05-16 16:42:39 -0400

    update test_model

--- test_model.R
+++ test_model.R
@@ -1,11 +1,10 @@
-
+library(bmstdr)
 library(raster)
 library(spdep)
+library(sf)
 
 p_pe <- tar_read(pe_ts_test)
-
 p_pe$mean_temp <- as.numeric(p_pe$mean_temp)
-
 p_pe$Date <- as.Date(p_pe$Date)
 
 
@@ -13,12 +12,15 @@ p_pe$Date <- as.Date(p_pe$Date)
 #Polygons Data Frame
 tar_load(raw_geom_data_pe)
 
+raw_geom_data_pe <- as_Spatial(raw_geom_data_pe)
+
 #switch off spherical geometry
 sf_use_s2(FALSE)
 
 #Neigbhour's list
 wr_pe <- poly2nb(raw_geom_data_pe, row.names=seq(1:length(raw_geom_data_pe$GeoUID)), queen=FALSE) 
 wr_pe
+
 
 #Neighbours matrix
 w_pe <- nb2mat(wr_pe, style='B', zero.policy = TRUE) 
@@ -43,4 +45,10 @@ f_pe <-  production_in_division_X11.Agriculture.forestry.fishing.hunting.21.Mini
 M_pe <- Bcartime(formula=f_pe, data=p_pe, scol= "GeoUID", tcol="Date", 
                  W=w_pe, model="ar", family="gaussian", package="CARBayesST", 
                  N=Ncar, burn.in=burn.in.car, thin=thin)
+
+
+M_pe <- Bcartime(formula=f_pe, data=p_pe, scol= "GeoUID", tcol="Date", 
+                 W=w_pe, model="ar", family="gaussian", package="CARBayesST", 
+                 N=Ncar, burn.in=burn.in.car, thin=thin)
+
 
